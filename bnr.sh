39a48fda99d3479dfd9759f823172859502326bc
Kyoung Whan Choe <choe.kyoung@gmail.com>
2022-09-25 19:07:10 -0700

    Update bnr.sh

--- bnr.sh
+++ bnr.sh
@@ -2,4 +2,4 @@
 # Build n Run helper script
 set -xe
 docker build . -t fsdl-sdprj
-docker run --rm --name fsdl-sdprj --cpus=2 -p 6000:6000 --gpus '"device=1"' -it fsdl-sdprj
+docker run --rm -p 127.0.0.1:64000:6000 --gpus '"device=1"' -it fsdl-sdprj
